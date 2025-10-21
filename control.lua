5a3e30bce54f7cbec2525efd6d35cdee4231e102
LeonSkills <leonskillsho@gmail.com>
2025-10-21 23:56:03 +0200

    Better storage initialization

--- control.lua
+++ control.lua
@@ -19,19 +19,17 @@ local function on_player_selected_area(event)
       local new_position = {x = tile.position.x - min_x, y = tile.position.y - min_y}
       table.insert(tiles, {name = tile.name, position = new_position})
     end
-
-    storage[event.player_index] = {tiles = tiles, width = max_x - min_x + 1, height = max_y - min_y + 1}
+    storage.tile_templates[event.player_index] = {tiles = tiles, width = max_x - min_x + 1, height = max_y - min_y + 1}
   end
 end
 
 local function on_player_reverse_selected_area(event)
   local item = event.item
   local instant_build = false
-  if not storage then return end
-  if not storage[event.player_index] then return end
+  if not storage.tile_templates[event.player_index] then return end
   if item == planner_name then
     local player = game.players[event.player_index]
-    local template = storage[event.player_index]
+    local template = storage.tile_templates[event.player_index]
     local new_tiles = tile_patterns.repeat_pattern(template.tiles, template.width, template.height, event.area)
     if instant_build then
       event.surface.set_tiles(new_tiles, true, true, true, true, player)
@@ -40,9 +38,14 @@ local function on_player_reverse_selected_area(event)
         event.surface.create_entity {name = "tile-ghost", inner_name = tile.name, position = tile.position, force = player.force, player = player, raise_built = true}
       end
     end
-
   end
 end
+
+script.on_configuration_changed(function()
+  if not storage.tile_templates then
+    storage.tile_templates = {}
+  end
+end)
 
 script.on_event(defines.events.on_player_selected_area, on_player_selected_area)
 script.on_event(defines.events.on_player_alt_selected_area, on_player_selected_area)
